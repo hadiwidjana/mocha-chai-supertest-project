13e293dfec5a59e3b76c24465a2f4bce19fa719a
albertus.hadiwidjana <albertus.hadiwidjana@gmail.com>
2022-08-18 16:29:48 +0700

    user input now updated from JSON file

--- testcases/putTestcase.js
+++ testcases/putTestcase.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 const api = require('../api/fazztrackAPI');
 const jsonPostData = require('../data/create-user.json');
 const jsonSchemaPut = require('../schemas/put-user-schema.json');
+const updateData = require('../data/update-data.json');
 
 chai.use(require('chai-like'));
 chai.use(require('chai-things'));
@@ -27,24 +28,15 @@ describe('[@update-data] PUT testcases', async() => {
 
 
      it('[@positive1] update occupation and nationality data', async() => {
-        let occupationUpdate = 'Soccer Athlete'
-        let nationalityUpdate = 'Indonesian'
-        let response = await api.putUser(
-            {
-                "id": targetID,
-                "firstName": "Kamen",
-                "lastName": "Rider",
-                "age": 30,
-                "occupation": occupationUpdate,
-                "nationality": nationalityUpdate,
-                "hobbies": [
-                  "Riding a motorcycle"
-                ],
-                "gender": "MALE",
-                "createdDate": "2022-08-11T19:04:27.816",
-                "updatedDate": null
-              }
-        );
+        //update ID and other datas
+        updateData.id=targetID
+        let occupationUpdate = "Soccer Player"
+        let nationalityUpdate = "Indonesian"
+        updateData.occupation = occupationUpdate
+        updateData.nationality = nationalityUpdate
+        
+     
+        let response = await api.putUser(updateData);
         expect(response.status).to.equal(200); //status code check
         expect(response.body.id).to.equal(targetID); //ID check
         expect(response.body).to.contain.like({occupation: occupationUpdate, nationality: nationalityUpdate}); //data check
@@ -52,75 +44,33 @@ describe('[@update-data] PUT testcases', async() => {
     });
 
     it('[@negative1] fail to update when age is 0', async() => {
-        let occupationUpdate = 'Soccer Athlete'
-        let nationalityUpdate = 'Indonesian'
-        let response = await api.putUser(
-            {
-                "id": targetID,
-                "firstName": "Kamen",
-                "lastName": "Rider",
-                "age": 0,
-                "occupation": occupationUpdate,
-                "nationality": nationalityUpdate,
-                "hobbies": [
-                  "Riding a motorcycle"
-                ],
-                "gender": "MALE",
-                "createdDate": "2022-08-11T19:04:27.816",
-                "updatedDate": null
-              }
-        );
+        updateData.age = 0;
+        let response = await api.putUser(updateData);
         expect(response.status).to.equal(400); //status code check
         expect(response.body.errorCode).to.equal('ER-03'); //Error code check
         expect(response.body).to.contain.like({"message": "you must specify data for firstname, lastName, age, occupation, nationality, hobbies (at least 1), and gender"}); //data check
+
+        updateData.age = 30;
     });
 
     it('[@negative2] fail to update when hobbies is empty array', async() => {
-        let occupationUpdate = 'Soccer Athlete'
-        let nationalityUpdate = 'Indonesian'
-        let response = await api.putUser(
-            {
-                "id": targetID,
-                "firstName": "Kamen",
-                "lastName": "Rider",
-                "age": 30,
-                "occupation": occupationUpdate,
-                "nationality": nationalityUpdate,
-                "hobbies": [
-                  
-                ],
-                "gender": "MALE",
-                "createdDate": "2022-08-11T19:04:27.816",
-                "updatedDate": null
-              }
-        );
+        updateData.hobbies.length=0;
+        let response = await api.putUser(updateData);
         expect(response.status).to.equal(400); //status code check
         expect(response.body.errorCode).to.equal('ER-03'); //Error code check
         expect(response.body).to.contain.like({"message": "you must specify data for firstname, lastName, age, occupation, nationality, hobbies (at least 1), and gender"}); //data check
+
+        updateData.hobbies=["Riding a motorcycle"];
     });
 
     it('[@negative3] fail to update when id is null', async() => {
-        let occupationUpdate = 'Soccer Athlete'
-        let nationalityUpdate = 'Indonesian'
-        let response = await api.putUser(
-            {
-                "id": null,
-                "firstName": "Kamen",
-                "lastName": "Rider",
-                "age": 30,
-                "occupation": occupationUpdate,
-                "nationality": nationalityUpdate,
-                "hobbies": [
-                  "Riding a motorcycle"
-                ],
-                "gender": "MALE",
-                "createdDate": "2022-08-11T19:04:27.816",
-                "updatedDate": null
-              }
-        );
+        updateData.id = null;
+        let response = await api.putUser(updateData);
         expect(response.status).to.equal(404); //status code check
         expect(response.body.errorCode).to.equal('ER-01'); //Error code check
         expect(response.body).to.contain.like({"message": "user not found"}); //data check
+
+        updateData.id = targetID;
     })
 
 
